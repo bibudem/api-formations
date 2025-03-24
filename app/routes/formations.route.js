8c37edcd82a212ca983ff1a07cb66300fea1233f
Christian Rémillard <christian.remillard@gmail.com>
2025-03-24 15:25:58 -0400

    feat: Ajout d'une nouvelle route `/`

--- app/routes/formations.route.js
+++ app/routes/formations.route.js
@@ -4,28 +4,26 @@ import config from 'config'
 import EventService from '../lib/event-service.js'
 import console from '../lib/console.js'
 
-const formationsRouter = new Router();
+const formationsRouter = new Router()
 
 const eventService = new EventService({
   calendarId: config.get('libCalApi.calendarId'),
-});
+})
 
-formationsRouter.param('type', (request, response, next, type) => {
-  if (!['bib', 'discipline'].includes(type)) {
+function validateTypeParameter(request, response, next) {
+  if (!['bib', 'discipline'].includes(request.params.type)) {
     return next(Boom.notFound())
   }
   next()
-})
+}
 
-formationsRouter.get('/:type/:id', async (request, response, next) => {
+formationsRouter.get('/:type/:id', validateTypeParameter, async (request, response, next) => {
   console.debug(request.params)
   // const type = req.params.type === 'discipline' ? 'category' : 'campus';
   // const id = EventService.translateLibCalIdToApi(type, req.params.id);
 
-  const type = request.params.type;
-  const id = request.params.id;
-  const uid = `${type}:${id}`
-  console.log(uid)
+  const type = request.params.type
+  const id = request.params.id
 
   if (id) {
     return eventService
@@ -37,4 +35,11 @@ formationsRouter.get('/:type/:id', async (request, response, next) => {
   next(Boom.notFound())
 })
 
-export default formationsRouter;
+formationsRouter.get('/', async (request, response, next) => {
+  return eventService
+    .getEvents()
+    .then(events => response.send(events))
+    .catch(error => next(error))
+})
+
+export default formationsRouter
